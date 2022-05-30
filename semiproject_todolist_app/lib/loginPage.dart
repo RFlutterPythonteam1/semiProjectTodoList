ac6714e90d8ad5e54f06a56562bbf94a3e886de2
HyeogDo <peterhd@daum.net>
2022-05-30 18:10:11 +0900

    [update] 2022.05.30 로그인페이지 테스트용 버튼 제거

--- semiproject_todolist_app/lib/loginPage.dart
+++ semiproject_todolist_app/lib/loginPage.dart
@@ -12,23 +12,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "for Test",
-        ),
-      ),
-      body: ElevatedButton(
-        onPressed: () {
-          TodoList.listId = 1;
-          TodoList.userId = "aa";
-          TodoList.category = "bb";
-          TodoList.content = "cc";
-          Navigator.pushNamed(context, "/listEditingPage");
-        },
-        child: const Text(
-          "수정하기",
-        ),
-      ),
+
     );
   }
 }
